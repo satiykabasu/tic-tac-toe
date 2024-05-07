710f434f856630ff71f43b8717b7681be0337ada
satiykabasu <ba.su95@proton.me>
2024-05-07 17:34:39 +0530

    tic tac toe game in react

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,22 @@
+
+
+import './App.css'
+import BoardValue from './component/BoardValue'
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="app">
+
+      <BoardValue boardSize={4}/>
+   
+        
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ tic-tac-toe/src/component/BoardValue.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import useTictacToe from '../hooks/use-tic-tac-toe';
+
+
+
+function BoardValue() {
+
+    const {board, handleClick, resetGame, getStatusMessage} = useTictacToe();
+
+  return (
+    <div className="game">
+      <div className="status">
+        {getStatusMessage()}
+        <button className="reset-button" onClick={resetGame}>
+          Reset Game
+        </button>
+      </div>
+
+      <div className="board">
+        {board.map((b, index) => {
+          return (
+            <button
+              className="cell"
+              key={index}
+              onClick={() => handleClick(index)}
+              disabled={b !== null}
+            >
+              {b}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default BoardValue
